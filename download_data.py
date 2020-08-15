5079b5bbd0151712cc6960b64b878cd2249865f1
John Rangel <rangel.john.j@gmail.com>
2020-08-15 15:34:41 -0500

    Reorganized things.

--- download_data.py
+++ download_data.py
@@ -13,10 +13,10 @@ import subprocess
 # -------------------------------------------------------------
 def create_daily_dir():
 	today = datetime.date.today().strftime("%Y-%m-%d")
-	subprocess.Popen(["mkdir", "data/%s" %today])
+	subprocess.Popen(["mkdir", "bin/data-%s" %today])
 
 # -------------------------------------------------------------
-# Local functions.
+# download data.
 # -------------------------------------------------------------
 dshs_urls = [
 	"https://dshs.texas.gov/coronavirus/TexasCOVID19DailyCountyCaseCountData.xlsx",
@@ -28,6 +28,6 @@ today = datetime.date.today().strftime("%Y-%m-%d")
 for url in dshs_urls:
 	spreadsheet = url.split("/")[-1]
 	r = requests.get(url)
-	with open("data/%s/%s" %(today, spreadsheet), "wb") as of:
+	with open("bin/data-%s/%s" %(today, spreadsheet), "wb") as of:
 		of.write(r.content)
 
